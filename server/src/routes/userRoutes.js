56740e2480bb8d851b4d9e4cd671e60377d0b956
Begli <velikijbegli@gmail.com>
2026-09-03 02:19:46 +0500

    initial commit

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+import { prisma } from '../../generated/lib/prisma.js';
+
+const createUserRoute = Router().post("/createuser", async (req, res) => {
+    try {
+        const { name, email } = req.body;
+        console.log(name, email)
+        
+        const user = await prisma.user.create({
+            data: { name, email},
+        });
+        
+        res.status(201).json({
+            data: user
+        })
+    } catch (error) {
+        res.status(500).json({ error: "Failed to create user", details: error.message })
+    }
+});
+
+const getUsers = Router().get("/getusers", async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+
+        res.status(200).json({ users });
+    } catch (error) {
+        res.status(500).json({
+            message: "Couldnt get users",
+            details: error.message
+        });
+    };
+});
+
+const getSpecifiedUser = Router().get("/getspecified/:id", async (req, res) => {
+    try {
+        const userId = req.params.id;
+        
+        const user = prisma.user.findUnique({
+            where: { id: userId }
+        });
+
+        res.status(200).json({
+            user,
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            message: "Couldnt get the user by id",
+            error: error?.message
+        });
+    };
+});
+
+export { createUserRoute, getUsers, getSpecifiedUser };
---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from "express";
+import { createUserRoute, getSpecifiedUser, getUsers } from './routes/userRoutes.js';
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+app.use("/", await createUserRoute);
+app.use("/", await getUsers);
+app.use("/", await getSpecifiedUser);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
