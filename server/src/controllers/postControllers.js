9c4bc65db3ee67ef0ac61c952d3366698a4801c6
Begli <velikijbegli@gmail.com>
2026-09-04 23:57:23 +0500

    crud for post

--- server/src/controllers/postControllers.js
+++ server/src/controllers/postControllers.js
@@ -25,10 +25,65 @@ const createPost = async (req, res) => {
     }
 };
 
-const getUserPosts = async (req, res) => {
+const getPost = async (req, res) => {
+    const postId = req.params.id;
     try {
-        const { userId } = req.params.id;
+        const post = await prisma.post.findUnique({
+            where: {id: postId }
+        });
 
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        res.status(200).json({ post });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+const updatePost = async (req, res) => {
+    const postId = req.params.id;
+    const userId = req.user.id;
+    const userRole = req.user.role;
+    const { title, details } = req.body;
+    try {
+        const post = await prisma.post.findUnique({
+            where: {id: postId }
+        });
+
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        const isAdmin = userRole === "ADMIN";
+        const isAuthor = post.authorId === userId;
+
+        if(!isAdmin && !isAuthor) {
+            return res.status(403).json({ message: "Access denied. You are not authorized to delete this post." });
+        }
+
+        await prisma.post.update({
+            where: { id: postId },
+            data: {
+                title,
+                details
+            }
+        });
+
+        return res.status(200).json({ 
+            message: 'Post successfully updated',
+            data: post
+        });
+
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+const getUserPosts = async (req, res) => {
+    const { userId } = req.params.id;
+    try {
         const posts = await prisma.post.findMany({
             where: { userId }
         })
@@ -43,4 +98,35 @@ const getUserPosts = async (req, res) => {
     }
 };
 
-export { createPost, getUserPosts };
+const deletePost = async (req, res) => {
+    const postId = req.params.id;
+    const userId = req.user.id;
+    const userRole = req.user.role;
+
+    try {
+        const post = await prisma.post.findUnique({
+            where: { id: postId }
+        });
+
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        const isAdmin = userRole === "ADMIN";
+        const isAuthor = post.authorId === userId;
+
+        if(!isAdmin && !isAuthor) {
+            return res.status(403).json({ message: "Access denied. You are not authorized to delete this post." });
+        }
+
+        await prisma.post.delete({
+            where: { id: postId }
+        });
+
+        return res.status(200).json({ message: 'Post successfully deleted' });
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+export { createPost, getPost, updatePost, getUserPosts, deletePost };
--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { prisma } from "../../generated/lib/prisma.js";
-import { createPost, getUserPosts } from "../controllers/postControllers.js";
+import { createPost, deletePost, getPost, getUserPosts, updatePost } from "../controllers/postControllers.js";
 import { authMiddleware, checkRole } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/create-post", authMiddleware, checkRole(["USER", "ADMIN"]), createPost);
+router.post("/create-post", authMiddleware, createPost);
+router.get("/get-post/:id", getPost);
+router.post("/update-post/:id", authMiddleware, updatePost);
 router.get("/get-user-posts/:id", getUserPosts);
+router.delete("/delete-post/:id", authMiddleware, deletePost);
 
 export default router;
