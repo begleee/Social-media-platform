61d60182878379f8923c00f71d4bcea64aed8c4d
Begli <velikijbegli@gmail.com>
2026-09-12 23:44:35 +0500

    feat: set up react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import { ThemeProvider } from "#components/theme-provider"
 import { ModeToggle } from "#components/mode-toggle";
+import { Link, Outlet } from "react-router";
 
 function App() {
 
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ModeToggle/>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <ModeToggle/>
+          </nav>
+          <main>
+            <Outlet/>
+          </main>
+        </div>
       </ThemeProvider>
     </>
   )
