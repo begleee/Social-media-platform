68feb9e5b781fb5efecc40775ca1b708baa28216
Begli <velikijbegli@gmail.com>
2026-09-17 16:50:19 +0500

    fix: fixed checking authentication

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuthStore } from '../store/authStore'
 import { Navigate, Outlet } from 'react-router';
+import ButtomTabBar from './ButtomTabBar';
 
 export default function ProtectedRoute() {
     const user = useAuthStore(state => state.user);
     const loading = useAuthStore(state => state.loading);
+    const checkAuth = useAuthStore(state => state.checkAuth);
 
-    if(loading) (<h1>Loading profile...</h1>)
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth]);
+
+    if(loading) {
+        return <h1>Loading profile...</h1>
+    }
     
     return user ? (
-        <Outlet/>
+        <>
+            <Outlet/>
+            <ButtomTabBar/>
+        </>
     ) : (
         <Navigate to="/login"/>
     )
